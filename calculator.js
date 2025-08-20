b62f58f79fa078270fa4ad7b70f879507324db12
Kabilashini-Uki <kabilashini.uki.stu@dreamspace.academy>
2025-08-20 16:30:22 +0530

    practice

---
+++ calculator.js
@@ -0,0 +1,24 @@
+var num1=25;
+var num2=50;
+var num3=80;
+
+function sum(a,b){
+    console.log("Addition =" +(a+b));
+
+}
+function sub(a,b){
+    console.log("Substraction ="+(a-b))
+}
+
+function div(a,b){
+    console.log("Divition =" +(a/b));
+}
+function multi(a,b){
+    console.log("Multiplication =" +(a*b));
+}
+sum(num1,num2);
+sub(num1,num2);
+div(num1,num2);
+multi(num1,num2);
+
+sum(num2,num3);
